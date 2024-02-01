6b52d5adec266b426e7c90cf624c67dad7ea49a6
Jess <jedeu@freenet.de>
2024-02-01 15:03:14 +0100

    Raum 2 angefangen, Orbs verteilt, Camera script angelegt, Hinweise

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float rotationSpeed = 5f;
+    public float maxVerticalAngle = 90f;
+    public float maxHorizontalAngle = 90f;
+
+    private float verticalRotation = 0f;
+    private float horizontalRotation = 0f;
+
+    void Update()
+    {
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+
+        horizontalRotation += mouseX * rotationSpeed;
+        horizontalRotation = Mathf.Clamp(horizontalRotation, -maxHorizontalAngle, maxHorizontalAngle);
+
+
+        verticalRotation -= mouseY * rotationSpeed;
+        verticalRotation = Mathf.Clamp(verticalRotation, -maxVerticalAngle, maxVerticalAngle);
+
+        transform.localRotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0f);
+    }
+}
---
+++ Assets/Scripts/Riddle2.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Riddle2 : MonoBehaviour
+{
+    public GameObject redOrb;
+    public GameObject orangeOrb;
+    public GameObject yellowOrb;
+    public GameObject greenOrb;
+    public GameObject blueOrb;
+    public GameObject purpleOrb;
+}
